79ee9418d2644c4deedc1a026e157c0972837b0d
Sammy <67427031+slpardes@users.noreply.github.com>
2023-02-10 15:20:24 -0500

    Add files via upload

---
+++ super_squares/README.md
@@ -0,0 +1,172 @@
+# Super Squares
+
+Super Squares is a tool that allows anyone to generate and randomly assign Super Bowl squares.
+
+## Prerequisites 
+
+Before you begin, ensure you have met the following requirements:
+
+- You have read the documentation
+
+## Installing Super Squares
+
+To install Super Squares, follow these steps on all OS devices:
+
+1. Install using the following command: `pip install supersquares`
+2. Import the package with: `from supersquares import squares`
+
+## Functions Overview 
+
+### Square Stats 
+
+The `square_stats()` function prints information about a created Squares instance. This information incluces the number of rows and columns. The default number of rows and columns is 10. There is no default value for the minimum or maximum number of squares per player. Unless otherwise specified, the team associated with the number of rows is the AFC and the NFC team aligns with the columns. The default cost per square is $5.00 and the payouts are 15% for Q1 and Q3, 30% for Q2, and 40% for Q4.
+
+The square_stats() function does not take any input. 
+
+### Add Player
+
+To add a player to the Squares instance, use the `add_player()` function. This function takes a player name as a string and the number of squares as an integer. Only unique names will be accepted. If there are minimum or maximum square requirements, they must be met. There must be enough available squares for a player to be added.
+
+### Assign Squares
+
+Use `assign_squares()` to give each player randomized squares corresponding to the number they purchased. This function does not take any input. 
+
+### Assign Empty Squares
+
+The `assign_empty_squares()` function can be used to randomly assign any blank squares to players already in the pool and does not take any variables as input. 
+
+### Get Player Squares
+
+Use the `print_squares()` function to generate a tabulated format of the squares and save the grid as a .csv file called "squares.csv". No input is needed to print the squares.
+
+### Payout 
+
+Each player has squares associated with their name. To get the squares assigned to a specific player, user the `get_player_squares()` function, which takes the player name string as input.
+
+## Usage Example
+
+1. Create a Squares() instance with the desired parameters and store it in a variable.
+	Input: 
+	```
+	squares_example = Squares(cost_per_square = 10, 
+							team_a = "Pawnee", 
+							team_b = "Eagleton",
+							min_squares = 1,
+							q1 = 10, 
+							q3 = 10, 
+							q4 = 50)
+	```
+2. View the specifications of the grid 
+	Input: 
+	```
+	squares_example.square_stats()
+	```
+	Output: 
+	```
+	Number of rows: 10
+	Number of columns: 10
+	Minimum number of squares: 1
+	Maximum number of squares: None
+	Team A: Pawnee
+	Team B: Eagleton
+	Cost per square: $10
+	Total pot: $1000
+		Q1 payout: 10%, $100.0
+		Halftime payout: 30%, $300.0
+		Q3 payout: 10%, $100.0
+		Final Score payout: 50%, $500.0
+	Available squares: 100
+	Unavailable squares: 0
+	```
+3. Add new users to the pool. Note: the entered number of squares must be available in the pool. Players must have unique names. 
+	Input: 
+	```
+	squares_example.add_player("Leslie", 10)
+	squares_example.add_player("Ben", 15)
+	squares_example.add_player("Ron", 10)
+	squares_example.add_player("Ann", 10)
+	squares_example.add_player("Donna", 15)
+	squares_example.add_player("April", 5)
+	squares_example.add_player("Tom", 10)
+	squares_example.add_player("Chris", 10)
+	squares_example.add_player("Andy", 10)
+	squares_example.add_player("Jerry", 5)
+	```
+4. Assign squares to all of the players at once 
+	Input: 
+	```
+	squares_example.assign_squares()
+	```
+5. Check the squares assigned to a specific user 
+	Input: 
+	```
+	squares_example.get_player_squares("Leslie")
+	```
+	Output: 
+	```
+	Leslie has the following squares: [(4, 9), (2, 8), (3, 9), (7, 6), (2, 6), (6, 8), (2, 2), (8, 2), (5, 3), (6, 6)]
+	```
+6. Print the grid
+	Input:
+	```
+	squares_example.print_squares()
+	```
+	Output: 
+	```
+			0           1           2           3           4           5           6           7           8           9
+			Eagleton    Eagleton    Eagleton    Eagleton    Eagleton    Eagleton    Eagleton    Eagleton    Eagleton    Eagleton
+	------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------
+	0       Ben         Ann         Donna       Chris       Andy        Chris       Ron         Ben         Chris       Donna
+	Pawnee
+	1       Ron         Tom         Tom         Chris       Ann         April       Ann         Andy        Ben         Ann
+	Pawnee
+	2       Ben         Chris       Leslie      Donna       Ron         Donna       Leslie      Ron         Leslie      April
+	Pawnee
+	3       Chris       Ann         Chris       Chris       Ben         Tom         Donna       Andy        Andy        Leslie
+	Pawnee
+	4       April       Andy        Donna       Ron         Donna       Jerry       Ann         Ron         Ben         Leslie
+	Pawnee
+	5       Donna       Ann         Ben         Leslie      Ben         Andy        Donna       April       Ben         Tom
+	Pawnee
+	6       Tom         Tom         Ben         Ben         Jerry       Tom         Leslie      Ron         Leslie      Donna
+	Pawnee
+	7       Chris       Donna       Ben         Ann         Ben         Andy        Leslie      Ron         Donna       Ron
+	Pawnee
+	8       Donna       Andy        Leslie      Tom         Jerry       April       Ron         Ann         Jerry       Ben
+	Pawnee
+	9       Tom         Andy        Donna       Andy        Chris       Tom         Donna       Ann         Jerry       Ben
+	Pawnee
+	```
+7. Enter the score at the end of each quarter 
+	Input:
+	```
+	squares_example.payout(7, 28, 1)
+	```
+	Output: 
+	```
+	The Q1 winner is Donna with the (7, 8) square. They win $100.0!
+	```
+
+## FAQ
+
+### What variables can be modified?
+- Number of rows (default = 10)
+- Number of columns (default = 10)
+- Minimum number of squares (default = None)
+- Maximum number of squares (default = None)
+- Team A Name (default = AFC)
+- Team B Name (default = NFC)
+- Cost per squares (default = $5.00)
+- Quarter 1 payout percentage (default = 15%)
+- Halftime payout percentage (default = 30%)
+- Quarter 3 payout percentage (default = 15%)
+- Final score payout percentage (default = 40%)
+
+### Why can't I manually choose squares?
+Accoring to [sportingnews.com](https://www.sportingnews.com/us/nfl/news/super-bowl-squares-grid-2023-best-numbers/ltur0ayv6wvsdkyjjk2cnmdo), the most frequently occuring squares are (0,0), (0, 3), (3, 0), (0, 7), (7, 0), (3, 7) and (7, 3). Squares are assigned randomly to fairly distribute squares with a greater chance of winning.
+
+## Contact
+If you want to contact me, you can reach me at slpardes@syr.edu.
+
+## License
+This project uses the following license: MIT License.
---
+++ super_squares/build/lib/supersquares/squares.py
@@ -0,0 +1,236 @@
+import random
+import tabulate
+
+
+class Squares:
+    """
+    The Squares class! 
+    
+    The number of rows and columns default to ten to create a 10x10 grid. 
+    It is optional to set the minimum or maximum number of squares a player can purchase. 
+    
+    By default, Team A (rows) will be the AFC and Team B (columns) will be the NFC. These values can be overwritten. 
+    
+    The adjustable cost per square is $5.00.
+    
+    The payout percentage are set to be 15% for quarters 1 and 3, 30% for quarter 2 (halftime), and 40% of the pot for quarter 4 (the final score). 
+    These values can be updated.
+    """
+
+    def __init__(
+        self,
+        n_rows=10,
+        n_cols=10,
+        min_squares=None,
+        max_squares=None,
+        team_a="AFC",
+        team_b="NFC",
+        cost_per_square=5,
+        q1=15,
+        q2=30,
+        q3=15,
+        q4=40,
+    ):
+
+        self.n_rows = n_rows
+        self.n_cols = n_cols
+        self.min_squares = min_squares
+        self.max_squares = max_squares
+        self.team_a = team_a
+        self.team_b = team_b
+        self.cost_per_square = cost_per_square
+        self.q1 = q1
+        self.q2 = q2
+        self.q3 = q3
+        self.q4 = q4
+
+        self.squares_pool = {(i, j): None for i in range(n_rows) for j in range(n_cols)}
+        self.squares_dict = {}
+        self.player_dict = {}
+
+        self.total_pot = self.cost_per_square * (self.n_rows * self.n_cols)
+
+        # check that manually entered quarter percentages add up to 100% exactly
+        if (q1 + q2 + q3 + q4) != 100:
+            print("Error: Quarter payouts must add up to 100%.")
+
+    def square_stats(self):
+        """
+        The square_stats() function prints information on a created Squares instance.
+        This information incluces the number of rows and columns. The default number of rows and columns is 10.
+        There is no default value for the minimum or maximum number of squares per player.
+        Unless otherwise specified, the team associated with the number of rows is the AFC. 
+        The NFC team aligns with the columns.
+        The default cost per square is $5.00 and the payouts are 15% for Q1 and Q3, 30% for Q2, and 40% for Q4.
+        """
+        print(f"Number of rows: {self.n_rows}")
+        print(f"Number of columns: {self.n_cols}")
+        print(f"Minimum number of squares: {self.min_squares}")
+        print(f"Maximum number of squares: {self.max_squares}")
+        print(f"Team A: {self.team_a}")
+        print(f"Team B: {self.team_b}")
+        print(f"Cost per square: ${self.cost_per_square}")
+        print(f"Total pot: ${self.total_pot}")
+        print(f"\tQ1 payout: {self.q1}%, ${self.total_pot *(self.q1/100)}")
+        print(f"\tHalftime payout: {self.q2}%, ${self.total_pot *(self.q2/100)}")
+        print(f"\tQ3 payout: {self.q3}%, ${self.total_pot *(self.q3/100)}")
+        print(f"\tFinal Score payout: {self.q4}%, ${self.total_pot *(self.q4/100)}")
+        print(f"Available squares: {len(self.squares_pool)}")
+        print(f"Unavailable squares: {len(self.squares_dict)}")
+
+    def add_player(self, name: str, n_squares: int):
+        """
+        To add a player to the Squares instance, use the add_player() function.
+        This function takes a player name as a string and the number of squares as an integer. 
+        Only unique names will be accepted. 
+        If there are minimum or maximum square requirements, they must be met.
+        There must be enough available squares for a player to be added.
+        """
+
+        # check that the player name is unique
+        if name in self.player_dict:
+            print(
+                f"Error: {name} has already been added to the pool. Please enter a unique player name."
+            )
+            return
+
+        # check that the player meets the optional minimum squares criteria
+        elif self.min_squares != None and n_squares < self.min_squares:
+            print(
+                f"Error: {name} has not met the minimum number of squares for this pool. The minimum number of squares per player is {self.min_squares}"
+            )
+            return
+
+        # check that the player meets the optional maximum squares criteria
+        elif self.max_squares != None and n_squares > self.max_squares:
+            print(
+                f"Error: {name} has exceeded the maximum number of squares for this pool. The maximum number of squares per player is {self.max_squares}"
+            )
+            return
+
+        # check that there are enough available squares for the player to join the pool
+        elif (sum(self.player_dict.values()) + n_squares) > (self.n_rows * self.n_cols):
+            print(
+                f"Error: Not enough available squares. {name} asked for {n_squares} squares but there are {(self.n_rows * self.n_cols) - sum(self.player_dict.values())} remaining squares."
+            )
+            return
+
+        # add the player to the pool with their desired number of squares
+        else:
+            player_squares = []
+            self.player_dict[name] = n_squares
+            print(f"{name} added to the pool with {n_squares} squares")
+        return
+
+    def assign_squares(self, assign_empty_squares=True):
+        """
+        Use assign_squares() to give each player randomized squares corresponding to the number they purchased.
+        
+        """
+        # for each player, assign them the number of random squares that they purchased
+        for player, n_squares in self.player_dict.items():
+
+            for i in range(n_squares):
+                square = random.choice(list(self.squares_pool.keys()))
+                self.squares_pool.pop(square)
+                self.squares_dict[square] = player
+        return
+
+    def assign_empty_squares(self):
+        player_list = list(self.player_dict.keys())
+
+        for i in range(len(self.squares_pool.keys())):
+            if len(player_list) == 0:
+                player_list = list(self.player_dict.keys())
+            else:
+                player = random.choice(player_list)
+                player_list.remove(player)
+
+            square = random.choice(list(self.squares_pool.keys()))
+
+            self.squares_pool.pop(square)
+            self.squares_dict[square] = player
+
+        return
+
+    def print_squares(self):
+
+        # create an empty grid using the number of rows and columns (optionally) specified
+        output = [[(None) for _ in range(self.n_rows)] for _ in range(self.n_cols)]
+
+        # order the squares dictionary
+        for key, value in sorted(self.squares_dict.items()):
+            i = key[0]
+            j = key[1]
+            output[i][j] = value
+
+        # append the team name to the index
+        rowIDs = [f"{i}\n{self.team_a}" for i in range(self.n_rows)]
+
+        # create a table from the assigned squares
+        table = tabulate(
+            output,
+            headers=[f"{i}\n{self.team_b}" for i in range(self.n_cols)],
+            showindex=rowIDs,
+            tablefmt="simple",
+        )
+
+        text_file = open("squares.csv", "w")
+        text_file.write(table)
+        text_file.close()
+
+        print(table)
+        return
+
+    def clear_squares(self):
+
+        # reset the assigned squares grid and reset the pool of squares
+        self.squares_pool = {
+            (i, j): None for i in range(self.n_rows) for j in range(self.n_cols)
+        }
+        self.squares_dict = {}
+        return
+
+    def get_player_squares(self, name):
+        if name not in self.player_dict:
+            print(
+                f"Error: Please enter a user who already has squares. {name} does not have any squares."
+            )
+            return
+        else:
+            # enter user name, get their squares
+            squares_list = []
+
+            for key, value in self.squares_dict.items():
+                if value == name:
+                    squares_list.append(key)
+
+            print(f"{name} has the following squares: {squares_list}")
+
+            return
+
+    ##########
+    # PAYOUT #
+    ##########
+    def payout(self, team_a_score: int, team_b_score: int, quarter: int):
+
+        # enter the score and the quarter to get the winner and payout
+        team_a_digit = int(repr(team_a_score)[-1])
+        team_b_digit = int(repr(team_b_score)[-1])
+
+        winner = self.squares_dict.get((team_a_digit, team_b_digit))
+
+        # calculate payout depending on the quarter
+
+        if quarter == 1:
+            payout = self.total_pot * (self.q1 / 100)
+        if quarter == 2:
+            payout = self.total_pot * (self.q2 / 100)
+        if quarter == 3:
+            payout = self.total_pot * (self.q3 / 100)
+        if quarter == 4:
+            payout = self.total_pot * (self.q4 / 100)
+
+        print(
+            f"The Q{quarter} winner is {winner} with the ({team_a_digit}, {team_b_digit}) square. They win ${payout}!"
+        )
